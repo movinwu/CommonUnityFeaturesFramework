9f7ab96262306f0a09b406416c9377c05d70ebce
movinwu <movingmind@163.com>
2024-07-15 14:41:50 +0800

    优化物品池

--- Assets/CommonFeatures/Runtime/Scripts/Pool/GameObjectPool/CommonFeature_GameObjectPool.cs
+++ Assets/CommonFeatures/Runtime/Scripts/Pool/GameObjectPool/CommonFeature_GameObjectPool.cs
@@ -26,7 +26,7 @@ namespace CommonFeatures.Pool
         /// <summary>
         /// 缓存池
         /// </summary>
-        private Dictionary<(GameObject origin, Transform parent), List<ReferencePoolItem>> m_PoolDic = new Dictionary<(GameObject origin, Transform parent), List<ReferencePoolItem>>();
+        private Dictionary<(GameObject origin, Transform parent), (List<ReferencePoolItem> usingList, List<ReferencePoolItem> freeList)> m_PoolDic = new Dictionary<(GameObject origin, Transform parent), (List<ReferencePoolItem> usingList, List<ReferencePoolItem> freeList)>();
 
         /// <summary>
         /// 清理时间间隔计时器
@@ -50,7 +50,8 @@ namespace CommonFeatures.Pool
             m_PoolParent.parent = this.transform;
             m_PoolParent.localPosition = Vector3.zero;
             m_PoolParent.localRotation = Quaternion.identity;
-            m_PoolParent.localScale = Vector3.zero;
+            m_PoolParent.localScale = Vector3.one;
+            m_PoolParent.gameObject.SetActive(false);
 
             return base.Init();
         }
@@ -69,21 +70,84 @@ namespace CommonFeatures.Pool
 
                     //清理长时间没有使用的item
                     var realTime = Time.unscaledTime;
-                    foreach (var hashSet in m_PoolDic.Values)
+                    foreach (var listPair in m_PoolDic.Values)
                     {
-                        var array = hashSet.ToArray();
-                        for (int i = 0; i < array.Length; i++)
+                        var list = listPair.freeList;
+                        for (int i = list.Count - 1; i >= 0; i--)
                         {
-                            if (array[i].IsDestroy(realTime))
+                            if (list[i].IsDestroy(realTime))
                             {
-                                var poolItem = array[i];
+                                var poolItem = list[i];
+                                GameObject.Destroy(poolItem.Item);
                                 poolItem.Item = null;
                                 m_FreeItemList.Add(poolItem);
-                                hashSet.Remove(poolItem);
+                                list.RemoveAt(i);
                             }
                         }
                     }
                 }
+            }
+        }
+
+        /// <summary>
+        /// 从池中取用
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="parent"></param>
+        /// <param name="setIdetityTransform">设置标准transform参数</param>
+        /// <returns></returns>
+        public GameObject Acquire(GameObject origin, Transform parent, bool setIdetityTransform = true)
+        {
+            if (null == origin || null == parent)
+            {
+                return null;
+            }
+            var pair = (origin, parent);
+            if (!m_PoolDic.ContainsKey(pair))
+            {
+                m_PoolDic.Add(pair, (new List<ReferencePoolItem>(), new List<ReferencePoolItem>()));
+            }
+
+            var listPair = m_PoolDic[pair];
+            //还有没有使用的item
+            if (listPair.freeList.Count > 0)
+            {
+                var poolItem = listPair.freeList[listPair.freeList.Count - 1];
+                listPair.freeList.RemoveAt(listPair.freeList.Count - 1);
+                poolItem.Item.transform.SetParent(parent);
+                if (setIdetityTransform)
+                {
+                    poolItem.Item.transform.localPosition = Vector3.zero;
+                    poolItem.Item.transform.localRotation = Quaternion.identity;
+                    poolItem.Item.transform.localScale = Vector3.one;
+                }
+                listPair.usingList.Add(poolItem);
+                return poolItem.Item;
+            }
+            //所有item已使用
+            else
+            {
+                var go = GameObject.Instantiate(origin);
+                ReferencePoolItem poolItem = null;
+                if (m_FreeItemList.Count > 0)
+                {
+                    poolItem = m_FreeItemList[m_FreeItemList.Count - 1];
+                    m_FreeItemList.RemoveAt(m_FreeItemList.Count - 1);
+                }
+                else
+                {
+                    poolItem = new ReferencePoolItem();
+                }
+                poolItem.Item = go;
+                go.transform.SetParent(parent);
+                if (setIdetityTransform)
+                {
+                    go.transform.localPosition = Vector3.zero;
+                    go.transform.localRotation = Quaternion.identity;
+                    go.transform.localScale = Vector3.one;
+                }
+                listPair.usingList.Add(poolItem);
+                return go;
             }
         }
 
@@ -105,20 +169,31 @@ namespace CommonFeatures.Pool
             var pair = (origin, parent);
             if (!m_PoolDic.ContainsKey(pair))
             {
-                m_PoolDic.Add(pair, new List<ReferencePoolItem>());
+                m_PoolDic.Add(pair, (new List<ReferencePoolItem>(), new List<ReferencePoolItem>()));
             }
-            var list = m_PoolDic[pair];
-            GameObject[] result = new GameObject[count];
-            var time = Time.unscaledTime;
+            var listPair = m_PoolDic[pair];
             if (backAll)
             {
-                //从下标0开始
+                //存储所有item
+                var allPoolItem = new ReferencePoolItem[listPair.usingList.Count + listPair.freeList.Count];
+                for (int i = 0; i < listPair.usingList.Count; i++)
+                {
+                    allPoolItem[i] = listPair.usingList[i];
+                }
+                for (int i = 0; i < listPair.freeList.Count; i++)
+                {
+                    allPoolItem[i + listPair.usingList.Count] = listPair.freeList[i];
+                }
+                listPair.usingList.Clear();
+                listPair.freeList.Clear();
+
+                //重新分配
                 for (int i = 0; i < count; i++)
                 {
-                    GameObject go = null;
-                    if (list.Count <= i)
+                    //不足,需要新建
+                    if (allPoolItem.Length <= i)
                     {
-                        go = GameObject.Instantiate(origin);
+                        var go = GameObject.Instantiate(origin);
                         ReferencePoolItem item = null;
                         if (m_FreeItemList.Count > 0)
                         {
@@ -129,77 +204,97 @@ namespace CommonFeatures.Pool
                         {
                             item = new ReferencePoolItem();
                         }
-                        item.AddTime = time;
+                        go.transform.SetParent(parent);
+                        if (setIdetityTransform)
+                        {
+                            go.transform.localPosition = Vector3.zero;
+                            go.transform.localRotation = Quaternion.identity;
+                            go.transform.localScale = Vector3.one;
+                        }
                         item.Item = go;
-                        item.Using = true;
-                        list.Add(item);
+                        listPair.usingList.Add(item);
                     }
                     else
                     {
-                        var item = list[i];
-                        item.Using = true;
-                        item.AddTime = time;
-                        go = item.Item;
+                        var item = allPoolItem[i];
+                        var go = item.Item;
+                        go.transform.SetParent(parent);
+                        if (setIdetityTransform)
+                        {
+                            go.transform.localPosition = Vector3.zero;
+                            go.transform.localRotation = Quaternion.identity;
+                            go.transform.localScale = Vector3.one;
+                        }
+                        listPair.usingList.Add(item);
                     }
-                    go.transform.SetParent(parent);
-                    if (setIdetityTransform)
-                    {
-                        go.transform.localPosition = Vector3.zero;
-                        go.transform.localRotation = Quaternion.identity;
-                        go.transform.localScale = Vector3.one;
-                    }
-                    result[i] = go;
                 }
-                //放回所有其他item
-                for (int i = count; i < list.Count; i++)
+                var time = Time.unscaledTime;
+                for (int i = count; i < allPoolItem.Length; i++)
                 {
-                    var item = list[i];
-                    item.Using = false;
-                    item.AddTime = time;
+                    allPoolItem[i].AddTime = time;
+                    allPoolItem[i].Item.transform.SetParent(m_PoolParent);
+                    listPair.freeList.Add(allPoolItem[i]);
                 }
+                return listPair.usingList.Select(x => x.Item).ToArray();
             }
             else
             {
-                int findedIndex = 0;
-                if (findedIndex < result.Length)
+                var result = new GameObject[count];
+                for (int i = 0; i < count; i++)
                 {
-                    //遍历所有没有使用的item
-                    for (int i = 0; i < list.Count; i++)
-                    {
-                        if (!list[i].Using)
-                        {
-                            list[i].Using = true;
-                            list[i].Item.transform.SetParent(parent);
-                            result[findedIndex] = list[i].Item;
-                            findedIndex++;
-                        }
-                        if (findedIndex >= result.Length)
-                        {
-                            break;
-                        }
-                    }
-                }
-                if (findedIndex < result.Length)
-                {
-                    //创建不足的item
-                    for (int i = findedIndex; i < result.Length; i++)
+                    //不足,需要新建
+                    if (listPair.freeList.Count == 0)
                     {
                         var go = GameObject.Instantiate(origin);
+                        ReferencePoolItem item = null;
+                        if (m_FreeItemList.Count > 0)
+                        {
+                            item = m_FreeItemList[m_FreeItemList.Count - 1];
+                            m_FreeItemList.RemoveAt(m_FreeItemList.Count - 1);
+                        }
+                        else
+                        {
+                            item = new ReferencePoolItem();
+                        }
+                        go.transform.SetParent(parent);
+                        if (setIdetityTransform)
+                        {
+                            go.transform.localPosition = Vector3.zero;
+                            go.transform.localRotation = Quaternion.identity;
+                            go.transform.localScale = Vector3.one;
+                        }
+                        item.Item = go;
+                        listPair.usingList.Add(item);
+                        result[i] = go;
+                    }
+                    else
+                    {
+                        var item = listPair.freeList[listPair.freeList.Count - 1];
+                        listPair.freeList.RemoveAt(listPair.freeList.Count - 1);
+                        listPair.usingList.Add(item);
+                        var go = item.Item;
+                        go.transform.SetParent(parent);
+                        if (setIdetityTransform)
+                        {
+                            go.transform.localPosition = Vector3.zero;
+                            go.transform.localRotation = Quaternion.identity;
+                            go.transform.localScale = Vector3.one;
+                        }
+                        result[i] = go;
                     }
                 }
+                return result;
             }
-            return result;
         }
 
         /// <summary>
         /// 归还
         /// </summary>
         /// <param name="item"></param>
-        public void Back(IReference item)
+        public void Back(GameObject origin, Transform parent, GameObject item)
         {
-            if (m_AllHoldReference.Contains(item))
+            if (null == origin || null == parent)
             {
-                CommonLog.LogError("向缓存池中重复归还元素");
                 return;
             }
             //元素没有持有时间,无需归还
@@ -207,27 +302,121 @@ namespace CommonFeatures.Pool
             {
                 return;
             }
-            var type = item.GetType();
-            if (!m_PoolDic.ContainsKey(type))
+            var pair = (origin, parent);
+            if (!m_PoolDic.ContainsKey(pair))
             {
-                m_PoolDic.Add(type, new HashSet<ReferencePoolItem>());
+                m_PoolDic.Add(pair, (new List<ReferencePoolItem>(), new List<ReferencePoolItem>()));
             }
-            var hashSet = m_PoolDic[type];
-            ReferencePoolItem poolItem = null;
-            if (m_FreeItemList.Count == 0)
+
+            var listPair = m_PoolDic[pair];
+            //查找已归还元素
+            for (int i = 0; i < listPair.freeList.Count; i++)
             {
-                poolItem = new ReferencePoolItem();
+                if (listPair.freeList[i].Item.Equals(item))
+                {
+                    CommonLog.LogWarning($"物品 {item.name} 已经归还,不能重复归还");
+                    return;
+                }
+            }
+
+            var time = Time.unscaledTime;
+            //查找要归还的元素
+            for (int i = 0; i < listPair.usingList.Count; i++)
+            {
+                if (listPair.usingList[i].Item.Equals(item))
+                {
+                    var usingItem = listPair.usingList[i];
+                    usingItem.AddTime = time;
+                    usingItem.Item.transform.SetParent(m_PoolParent);
+                    listPair.freeList.Add(usingItem);
+                    return;
+                }
+            }
+
+            //新元素
+            ReferencePoolItem poolItem = null;
+            if (m_FreeItemList.Count > 0)
+            {
+                poolItem = m_FreeItemList[m_FreeItemList.Count - 1];
+                m_FreeItemList.RemoveAt(m_FreeItemList.Count - 1);
             }
             else
             {
-                var index = m_FreeItemList.Count - 1;
-                poolItem = m_FreeItemList[index];
-                m_FreeItemList.RemoveAt(index);
+                poolItem = new ReferencePoolItem();
             }
             poolItem.Item = item;
-            poolItem.AddTime = Time.unscaledTime;
-            hashSet.Add(poolItem);
-            item.Reset();
+            poolItem.AddTime = time;
+            item.transform.SetParent(m_PoolParent);
+            listPair.freeList.Add(poolItem);
+            return;
+        }
+
+        /// <summary>
+        /// 归还
+        /// </summary>
+        /// <param name="items"></param>
+        public void Back(GameObject origin, Transform parent, GameObject[] items)
+        {
+            if (null == origin || null == parent)
+            {
+                return;
+            }
+            //元素没有持有时间,无需归还
+            if (HoldDuration <= 0)
+            {
+                return;
+            }
+            var pair = (origin, parent);
+            if (!m_PoolDic.ContainsKey(pair))
+            {
+                m_PoolDic.Add(pair, (new List<ReferencePoolItem>(), new List<ReferencePoolItem>()));
+            }
+
+            var listPair = m_PoolDic[pair];
+            var time = Time.unscaledTime;
+            for (int j = 0; j < items.Length; j++)
+            {
+                var item = items[j];
+                //查找已归还元素
+                for (int i = 0; i < listPair.freeList.Count; i++)
+                {
+                    if (listPair.freeList[i].Item.Equals(item))
+                    {
+                        CommonLog.LogWarning($"物品 {item.name} 已经归还,不能重复归还");
+                        continue;
+                    }
+                }
+
+                //查找要归还的元素
+                for (int i = 0; i < listPair.usingList.Count; i++)
+                {
+                    if (listPair.usingList[i].Item.Equals(item))
+                    {
+                        var usingItem = listPair.usingList[i];
+                        usingItem.AddTime = time;
+                        usingItem.Item.transform.SetParent(m_PoolParent);
+                        listPair.freeList.Add(usingItem);
+                        continue;
+                    }
+                }
+
+                //新元素
+                ReferencePoolItem poolItem = null;
+                if (m_FreeItemList.Count > 0)
+                {
+                    poolItem = m_FreeItemList[m_FreeItemList.Count - 1];
+                    m_FreeItemList.RemoveAt(m_FreeItemList.Count - 1);
+                }
+                else
+                {
+                    poolItem = new ReferencePoolItem();
+                }
+                poolItem.Item = item;
+                poolItem.AddTime = time;
+                item.transform.SetParent(m_PoolParent);
+                listPair.freeList.Add(poolItem);
+                continue;
+            }
         }
 
         /// <summary>
@@ -244,11 +433,6 @@ namespace CommonFeatures.Pool
             /// 保存的元素
             /// </summary>
             public GameObject Item { get; set; }
-
-            /// <summary>
-            /// 是否正在使用中
-            /// </summary>
-            public bool Using { get; set; }
 
             /// <summary>
             /// 是否销毁
